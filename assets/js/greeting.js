048893df3d936aea1693c03abeed974252399f8b
mbutsk <maratik1036@gmail.com>
2024-11-15 13:59:57 +0000

    my config v2

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -13,11 +13,11 @@ const gree3 = `${CONFIG.greetingAfternoon}\xa0`;
 const gree4 = `${CONFIG.greetingEvening}\xa0`;
 
 if (hour >= 23 || hour < 6) {
-	document.getElementById('greetings').innerText = gree1 + name;
+	document.getElementById('greetings').innerText = gree1.replace("$", name);
 } else if (hour >= 6 && hour < 12) {
-	document.getElementById('greetings').innerText = gree2 + name;
+	document.getElementById('greetings').innerText = gree2.replace("$", name);
 } else if (hour >= 12 && hour < 17) {
-	document.getElementById('greetings').innerText = gree3 + name;
+	document.getElementById('greetings').innerText = gree3.replace("$", name);
 } else {
-	document.getElementById('greetings').innerText = gree4 + name;
+	document.getElementById('greetings').innerText = gree4.replace("$", name);
 }
